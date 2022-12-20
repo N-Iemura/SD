9bf55352ee8bbc7bb1e178c88a1c7b32a3fcb819
iemura <79625740+N-Iemura@users.noreply.github.com>
2022-12-20 13:37:16 +0900

    Add files via upload

--- sd_sample_pkg/scripts/teleop.py
+++ sd_sample_pkg/scripts/teleop.py
@@ -3,6 +3,7 @@ import rospy
 
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Joy
+from std_msgs.msg import String
 
 import sys, select, termios, tty
 
@@ -14,6 +15,7 @@ MAX_ANG_Z = 0.5
 usage = """
 usage:
 publish linear/angular velocity
+1 2
     w
 a   s   d
     x
@@ -22,6 +24,8 @@ x...go back
 a...rotate 
 d...rotate
 s...stop
+1...Left_Mode
+2...Right_Mode
 
 q...quit
 """
@@ -45,6 +49,7 @@ def main():
     print(usage)
     rospy.init_node('teleop_key')
     pub = rospy.Publisher('/cmd_vel', Twist, queue_size=5)
+    pub_cmd = rospy.Publisher('/cmd_LR', String, queue_size=1)
 
     while not rospy.is_shutdown():
         key = getKey(settings)
@@ -66,11 +71,25 @@ def main():
         elif key == "s":
             vel = 0.
             omega = 0.
+        elif key == "1":
+            cmd = String()
+            cmd.data = "Left"
+            pub_cmd.publish(cmd)
+            rospy.loginfo(cmd)
+            print("Left_Mode")
+        elif key == "2":
+            cmd = String()
+            cmd.data = "Right"
+            print("Right_Mode")
+            pub_cmd.publish(cmd)
 
+
+        
         twist = Twist()
         twist.linear.x = vel; twist.linear.y = 0; twist.linear.z = 0
         twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = omega
         pub.publish(twist)
+
 
         termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
 
